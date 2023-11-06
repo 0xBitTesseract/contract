b661f68f7a0f5240a66e83127941e59902911c15
hnustfeng <hnustwhf@163.com>
2023-11-06 14:10:42 +0800

    delete

--- interfaces/IPriceOracleGetter.sol
+++
@@ -1,7 +0,0 @@
-//SPDX-License-Identifier:MIT
-
-pragma solidity ^0.8.9;
-
-interface IPriceOracleGetter {
-    function getAssetPrice(address _asset) external view returns (uint256);
-}
--- libraries/EthAddressLib.sol
+++
@@ -1,8 +0,0 @@
-//SPDX-License-Identifier:MIT
-pragma solidity ^0.8.9;
-
-library EthAddressLib {
-    function ethAddress() internal pure returns (address) {
-        return 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
-    }
-}
